53441e5481c9e38943d7b86afa0eded5c44b4a96
Bryan Zuñiga <bzuniga97@bitbucket.org>
2023-08-23 21:52:12 -0600

    first commit - navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./components/Nav"
+function App() {
+  return (
+    <>
+    <Nav />
+    <div className="bg-gray-900 w-full h-screen"></div>
+    </>
+  )
+}
+
+export default App
