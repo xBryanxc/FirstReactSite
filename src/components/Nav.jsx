3282aa7c270e9a16b7f0ec8887c02e60d7d4f8b1
Bryan Zuñiga <bzuniga97@bitbucket.org>
2023-08-28 10:55:37 -0600

    remove code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,7 +15,6 @@ function Nav() {
       <div className="bg-gray-900 pt-6">
         <nav
           className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
-          aria-label="Global"
         >
           <div className="flex items-center flex-1">
             <div className="flex items-center justify-between w-full md:w-auto">
